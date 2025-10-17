b97362adbef04b6aac0ad1486c55209ba14d1b9c
RaizyLevi <r0548419566@gmail.com>
2025-10-17 10:53:49 +0300

    my commit message

--- שיעור 2/Event.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication2
-{
-    public class Event
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public DateTime Start { get; set; }
-    }
-}
